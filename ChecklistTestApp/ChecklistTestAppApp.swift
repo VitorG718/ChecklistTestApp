fa5999661ed4119eb0d78e20d5249711bfe84d52
Vitor Souza <vitorx1280@gmail.com>
2022-08-17 16:27:35 -0300

    Initial Commit

---
+++ ChecklistTestApp/ChecklistTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistTestAppApp.swift
+//  ChecklistTestApp
+//
+//  Created by Vitor Souza on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
